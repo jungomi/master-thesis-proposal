fdb302358ca4686826d85bf0e2c9abaf974696cf
Michael Jungo <michaeljungo92@gmail.com>
2016-10-01 03:12:39 +0200

    Add test for model

---
+++ examples/react-counter/test/model.spec.js
@@ -0,0 +1,50 @@
+import test from 'ava';
+import sinon from 'sinon';
+import Model from '../src/model';
+
+const initialState = {
+  value: 0,
+  counting: false,
+  buttonDisabled: false
+};
+
+test.beforeEach(t => {
+  t.context.model = new Model(initialState);
+});
+
+test('present rejected data', t => {
+  t.context.model.notify = sinon.spy();
+  t.context.model.present(initialState);
+  t.false(t.context.model.notify.called);
+  t.deepEqual(t.context.model.store, initialState);
+});
+
+test('present accepted data', t => {
+  const nextState = {
+    value: 1,
+    counting: true,
+    buttonDisabled: false
+  };
+  t.context.model.notify = sinon.spy();
+  t.context.model.present(nextState);
+  t.true(t.context.model.notify.called);
+  t.deepEqual(t.context.model.store, nextState);
+});
+
+test('notify', t => {
+  const listen = sinon.spy();
+  t.context.model.subscribe(listen);
+  t.context.model.notify();
+  t.true(listen.calledOnce);
+});
+
+test('subscribe', t => {
+  const listen = () => {};
+  t.is(t.context.model.listeners.length, 0);
+  const unsubscribe = t.context.model.subscribe(listen);
+  t.is(t.context.model.listeners.length, 1);
+  t.true(t.context.model.listeners.includes(listen));
+  t.is(typeof unsubscribe, 'function');
+  unsubscribe();
+  t.is(t.context.model.listeners.length, 0);
+});
